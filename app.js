5119cb52ae27c846d86936fd28b0471acc2ca132
Nikita Petrashin <88317323+petrashin@users.noreply.github.com>
2022-09-30 02:38:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+import selfcore from "selfcore";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+var token = process.env.token
+var channel_id = process.env.channel_id
+
+const gateway = new selfcore.Gateway(token)
+
+gateway.on("message", m => {
+    if(m.channel_id === channel_id) {
+        let content = m.content ? m.content : { emdebs: [m.embeds[0]] };
+        console.log(content)
+    }
+})

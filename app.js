0eb032f2f0243f7eef3e814f55cbdbc85dfaf4b8
petrashin <petrashin02@bk.ru>
2022-10-01 13:38:06 +0300

    finished

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 import selfcore from "selfcore";
 import dotenv from 'dotenv';
+import TelegramBot from 'node-telegram-bot-api';
 
 dotenv.config()
 
-var token = process.env.token
+var auth_token = process.env.auth_token
+var bot_token = process.env.bot_token
 var channel_id = process.env.channel_id
+var chat_id = process.env.chat_id
 
-const gateway = new selfcore.Gateway(token)
+const gateway = new selfcore.Gateway(auth_token)
+const bot = new TelegramBot(bot_token)
 
 gateway.on("message", m => {
     if(m.channel_id === channel_id) {
         let content = m.content ? m.content : { emdebs: [m.embeds[0]] };
-        // TODO: send to tg chat via bot
-        // TODO: bot send to private channel
         console.log(content)
+        bot.sendMessage(chat_id, content)
     }
 })
